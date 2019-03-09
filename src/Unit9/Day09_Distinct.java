87c04d1efde1586c433f97c1f7b0c6be8f109a26
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-03-08 21:04:47 -0500

    first commit

---
+++ src/Unit9/Day09_Distinct.java
@@ -0,0 +1,33 @@
+package Unit9;
+import java.util.Scanner;
+
+
+public class Day09_Distinct {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] a = new int[10];
+        int count = 0;
+        System.out.println("Enter 10 numbers to find how many distinct values there are.");
+
+        for (int i = 0; i < 10; i++){
+
+            int num = input.nextInt();
+
+            boolean inList = false;
+            for (int j = 0; j < count; j++){
+                if(num == a[j]){
+                    inList = true;
+                    break;
+                }
+            }
+            if (!inList){
+                a[count++] = num;
+            }
+        }
+        for (int i = 0; i < count; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
--- src/Unit9/Ex6_4.java
+++ src/Unit9/Ex6_4.java
@@ -1,15 +1,24 @@
 package Unit9;
 
 public class Ex6_4 {
+
     public static void reverse(int num){
+
         String reverseDigits = "";
 
         // Implement question 3 https://bca.schoology.com/attachment/863481980/docviewer
 
-        while (num < 0){
+        while (num > 0){
 
-
+            reverseDigits += (num % 10);
+            num /= 10;
 
         }
+        System.out.println(reverseDigits);
+    }
+
+    public static void main(String[] args) {
+        reverse(123);
     }
 }
+
