b49434ba8442e8a1d366a546f29f7612148470ee
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-03-12 13:09:11 -0400

    first commit

--- src/Unit9/Ex7_1.java
+++ src/Unit9/Ex7_1.java
@@ -1,9 +1,28 @@
-/* package Unit9;
+package Unit9;
 
+import java.util.Scanner;
 public class Ex7_1 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.print("Enter the number of students: ");
-        int studentNum =
+        int studentNum = input.nextInt();
+
+        System.out.printf("Enter %d scores, separated by spaces\n", studentNum);
+        int[] studentGrades = new int[studentNum];
+        int highestGrade = -1;
+
+        for (int i = 0; i < studentGrades.length; i++){
+
+            studentGrades[i] = input.nextInt();
+
+        }
+        for (int num : studentGrades){
+
+        }
+
+
     }
 }
-*/
+
