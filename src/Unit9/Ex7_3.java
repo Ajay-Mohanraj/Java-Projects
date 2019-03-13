7fa40015509f11a985ba152b4a2131df5cc4d693
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-03-13 14:09:12 -0400

    first commit

---
+++ src/Unit9/Ex7_3.java
@@ -0,0 +1,26 @@
+package Unit9;
+import java.util.Scanner;
+
+public class Ex7_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the integers between 1 and 100: ");
+
+        int entry = -1;
+        int[] quantity = new int[101];
+
+        while (entry != 0){
+
+            entry = input.nextInt();
+            quantity[entry]++;
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            if (quantity[i] > 0) {
+                String output = i + " occurs " + quantity[i];
+                output += quantity[i] > 1 ? " times" : " time";
+                System.out.println(output);
+            }
+        }
+    }
+}
