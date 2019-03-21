8f7787f2204762ef2428c058ad90288e0a7fcbf6
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-03-21 12:55:44 -0400

    Latest commit of the java project

---
+++ src/unit10/day1/Tester.java
@@ -0,0 +1,10 @@
+package unit10.day1;
+
+public class Tester {
+    public static void main(String[] args) {
+        Point p1 = new Point(5, 3);
+        System.out.println(p1);
+        p1.dilate(2);
+        System.out.println(p1);
+    }
+}
