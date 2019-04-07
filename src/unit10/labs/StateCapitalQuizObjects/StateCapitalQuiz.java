1af71837e7d3408c3d2d9e68d9fee4388b843222
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-04-07 09:44:38 -0400

    Latest commit of the java project

--- src/unit10/labs/StateCapitalQuizObjects/StateCapitalQuiz.java
+++ src/unit10/labs/StateCapitalQuizObjects/StateCapitalQuiz.java
@@ -5,46 +5,55 @@ import java.io.FileNotFoundException;
 
 public class StateCapitalQuiz {
 
-    public static void main(String[] args) throws FileNotFoundException, NullPointerException{
-        JOptionPane.showMessageDialog(null, "This is State Capital Quiz");
-        String[] possibleFiles = {"states_small_test.txt", "states_east.txt", "states_south.txt", "states_west.txt", "states_central.txt", "states_all.txt"};
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] possibleChoices = {"I accept the challenge.", "I decline the challenge."};
+        String choice = (String) JOptionPane.showInputDialog(null, "Welcome to State Capital Quiz!",
+                "Input", JOptionPane.QUESTION_MESSAGE, null, possibleChoices, possibleChoices[0]);
 
-        String file = (String) JOptionPane.showInputDialog(null, "Which quiz file would you like to use?",
-                "Input", JOptionPane.QUESTION_MESSAGE, null, possibleFiles, possibleFiles[0]);
+        if (choice.equals("I decline the challenge.")) {
+            JOptionPane.showMessageDialog(null, "Bye!");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Welcome to State Capital Quiz!");
+            String[] possibleFiles = {"states_small_test.txt", "states_east.txt", "states_south.txt", "states_west.txt", "states_central.txt", "states_all.txt"};
 
-        if (file != null) {
-            StateCapitalList stateCapitalList = new StateCapitalList(file);
+            String file = (String) JOptionPane.showInputDialog(null, "Which quiz file would you like to use?",
+                    "Input", JOptionPane.QUESTION_MESSAGE, null, possibleFiles, possibleFiles[0]);
 
-            String input = "";
+            if (file != null) {
+                StateCapitalList stateCapitalList = new StateCapitalList(file);
 
-            while (input != null && stateCapitalList.statesRemaining() > 0){
+                String input = "";
 
                 int correct = 0;
                 int guesses = 0;
 
-                StateCapital state = stateCapitalList.getRandomState();
-                input = (String) JOptionPane.showInputDialog("What is the capital of " + (state.getState()) + "?");
-                if (state.getCapital().equals(input)){
-                    correct++;
-                    guesses++;
-                    stateCapitalList.remove(state);
+                while (input != null && stateCapitalList.statesRemaining() > 0) {
 
-                }
-                else if (input == null) {
-                    JOptionPane.showMessageDialog(null, "You named " + correct + " capitals in " + guesses + " guesses.");
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "Incorrect. The capital is " + state.getCapital() + ".");
-                }
+                    StateCapital state = stateCapitalList.getRandomState();
+                    input = (String) JOptionPane.showInputDialog("What is the capital of " + (state.getState()) + "?");
+                    if (state.getCapital().equals(input)) {
+                        correct++;
+                        guesses++;
+                        stateCapitalList.remove(state);
+                        JOptionPane.showMessageDialog(null, "Correct.");
+                        if (stateCapitalList.statesRemaining() == 0) {
+                            JOptionPane.showMessageDialog(null, "Congratulations. You named " + correct + " capitals in " + guesses + " guesses.");
+                        }
 
+                    }
+                    else if (input == null) {
+                        JOptionPane.showMessageDialog(null, "You named " + correct + " capitals in " + guesses + " guesses.");
+                    }
+                    else {
+                        guesses++;
+                        JOptionPane.showMessageDialog(null, "Incorrect. The capital is " + state.getCapital() + ".");
+                    }
+                }
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Bye!");
             }
         }
-        else {
-            JOptionPane.showMessageDialog(null, "Bye");
-        }
-        //while () {
-
-        //}
-
     }
 }
