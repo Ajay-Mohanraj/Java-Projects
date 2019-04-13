bb2b51c6a414c18a699ec3cd11b66ab22467321b
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2019-04-13 09:27:07 -0400

    Latest commit of the java project

--- src/unit10/lessons/RectangleObject/RectangleTester.java
+++ src/unit10/lessons/RectangleObject/RectangleTester.java
@@ -1,15 +1,23 @@
 package unit10.lessons.RectangleObject;
 
+import org.w3c.dom.css.Rect;
+
 public class RectangleTester {
     public static void main(String[] args) {
         Point p1 = new Point(5, 3);
         Point p2 = new Point(1, 6);
 
-        Rectangle r = new Rectangle(p1, p2);
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Rectangle(p1, p2);
+        shapes[1] = new Circle(new Point(7, 16), 3);
+        shapes[2] = new Circle(p2, 7);
 
-        System.out.println(r);
+        for (Shape s : shapes) {
+            System.out.println(s + "; Area = " + s.getArea());
+            s.translate(3, 0);
+            System.out.println(s + "; Area = " + s.getArea());
+        }
 
-        Circle c = new Circle(new Point(7, 16), 3);
-        System.out.println(c);
+
     }
 }
---
+++ src/unit10/usaco/section1_1/friday.java
@@ -0,0 +1,79 @@
+package unit10.usaco.section1_1;
+/*
+ID: ajamoh21
+LANG: JAVA
+TASK: friday
+*/
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class friday {
+	static final int START_YEAR = 1900;
+
+
+	public static boolean isLeapYear (int year)
+	{
+		//
+		// TODO: Implement this function to determine if a given year is a leap year.
+		//
+		if (year % 4 == 0 && year % 400 != 0) {
+			return (year % 100 != 0);
+		}
+		return (year % 4 == 0 && year % 400 == 0);
+	}
+	public static void main(String[] args) throws Exception {
+		// Represents the frequency of the 13th by day of the week.
+		// 1 = Sunday, 2 = Monday, ...
+        int count[] = new int[8];
+
+		BufferedReader input = new BufferedReader(new FileReader("friday.in"));
+		int endYear = Integer.parseInt(input.readLine()) + START_YEAR - 1;
+
+		// 1 = Sunday, 2 = Monday, ...
+		// January 1, 1900 was a Monday.
+        int firstDayOfMonth = 2;
+
+        // 1 = January, 2 = February, 3 = March, ...
+		// -9999 is just a filler value.  We are not using the 0 position of the array.
+        int[] days = new int[] { -9999, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        // Go progressively through every month from the start year through the end year.
+        for (int year = START_YEAR; year <= endYear; year++) {
+			if (isLeapYear(year)) {
+				days[2] = 29;
+			}
+            for (int month = 1; month <= 12; month++) {
+
+            	// TODO
+				// For each month, increment the appropriate position of the "count" array.
+				// Then, determine the first day of the next month (based upon the length of the current month
+
+
+				for (int i = 1; i <= days[month]; i++) {
+					// use increments and use base monday to calculate what day it is - use modulus 7
+					++firstDayOfMonth;
+					if (i == 13) {
+						++count[firstDayOfMonth % 7];
+					}
+
+				}
+				// firstDayOfMonth = days[month]
+
+			}
+        }
+
+		 PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
+
+        //
+		// TODO
+		// Print out the count array in the required format.
+		//
+		out.println(Arrays.toString(count));
+
+
+		 out.close();
+		 input.close();
+	}
+}
